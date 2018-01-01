96cf8bc358b985f720c29125a3ae3b933faef6f7
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-01-01 15:18:59 +0700

    Add SideBar component

---
+++ src/components/SideBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+  sidebar: {
+    width: 240,
+    height: "100vh",
+    backgroundColor: "#34495e",
+    padding: 24
+  }
+});
+
+const SideBar = ({ children }) => (
+  <div className={css(styles.sidebar)}>{children}</div>
+);
+
+SideBar.propTypes = {
+  children: PropTypes.node
+};
+
+export default SideBar;
